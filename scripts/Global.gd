a75a204ec209d1a7cf57a8c8b1b90fba3dd30f6b
David <d.weckerly@gmail.com>
2022-11-29 09:46:42 -0600

    new art and script updates

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -11,19 +11,19 @@ func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
 
-func goto_scene(path, playerPosition):
-	call_deferred("_deferred_goto_scene", path, playerPosition)
+func goto_scene(path, player_position):
+	call_deferred("_deferred_goto_scene", path, player_position)
 	
-func _deferred_goto_scene(path, playerPosition):
+func _deferred_goto_scene(path, player_position):
 	current_scene.free()
 	var s = ResourceLoader.load(path)
 	current_scene = s.instance()
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
-	load_player(playerPosition)
+	load_player(player_position)
 
-func load_player(playerPosition):
-	var playerScene = load("res://scenes/Player.tscn")
-	var player = playerScene.instance()
+func load_player(player_position):
+	var player_scene = load("res://scenes/Player.tscn")
+	var player = player_scene.instance()
 	get_tree().current_scene.add_child(player)
-	player.set_position(Vector2(playerPosition))
+	player.set_position(Vector2(player_position))
