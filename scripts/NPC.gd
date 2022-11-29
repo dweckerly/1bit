3fbdccaabb1c6e06ee99571241ebc1545abac2ea
David <d.weckerly@gmail.com>
2022-11-29 10:25:04 -0600

    new area and east-west transitions

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -2,5 +2,18 @@ extends StaticBody2D
 
 onready var animation_player = $AnimationPlayer
 
+var in_speaking_range = false
+
 func _ready():
 	animation_player.play("Idle")
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		in_speaking_range = true
+		print("in range to speak to NPC")
+
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Player":
+		in_speaking_range = false
+		print("out of range to speak to NPC")
--- scripts/Transitions.gd
+++ scripts/Transitions.gd
@@ -1,14 +1,23 @@
 extends Node
 
 const NORTH_SOUTH_OFFSET = 155
+const EAST_WEST_OFFSET = 290
 
 func change_scene(area, position):
 	var path = ""
 	var player_position = Vector2.ZERO
+	
 	if area == "0-South":
 		path = "res://scenes/world/area-1.tscn"
 		player_position = Vector2(position.x, position.y - NORTH_SOUTH_OFFSET)
 	elif area == "1-North":
 		path = "res://scenes/world/area-0.tscn"
 		player_position = Vector2(position.x, position.y + NORTH_SOUTH_OFFSET)
+	elif area == "1-East":
+		path = "res://scenes/world/area-2.tscn"
+		player_position = Vector2(position.x - EAST_WEST_OFFSET, position.y)
+	elif area == "2-West":
+		path = "res://scenes/world/area-1.tscn"
+		player_position = Vector2(position.x + EAST_WEST_OFFSET, position.y)
+	
 	Global.goto_scene(path, player_position)
